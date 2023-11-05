7427c23dfa1053b9dec4d3bc973dff86299092de
Daniil <pefilev007@outlook.com>
2023-11-05 17:24:53 +0100

    Funktion drucke_str

--- Aufgabe02/main.cpp
+++ Aufgabe02/main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <vector>
 
-void drucke(const std::vector<int>& arr, const size_t& size){
+void drucke_int(const std::vector<int>& arr, const size_t& size){
     /* Die Funktion drueckt die Array */
     // Sie ist const, da sie nicht veraendern wird
 
@@ -23,14 +23,29 @@ void betrag(std::vector<int>& arr, const size_t& size){
             arr[i] = -1 * arr[i];
         }
     }
+
+    drucke_int(arr, arr.size());
 }
 
+void drucke_str(const std::vector<std::string>& arr, const size_t& size){
+    /* Die Funktion drueckt die Array */
+    // Sie ist const, da sie nicht veraendern wird
+
+    // Drucken jeder Element
+    for(int i = 0; i < size; i++){
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl; // \n am Ende
+}
 
 int main(void){
     std::vector<int> arr{5,-4,2,-1,4};
+    std::vector<std::string> str_arr{"1. Wort", "2. Wort", "3. Wort"};
 
-    drucke(arr, arr.size()); // Teil 1
+
+    drucke_int(arr, arr.size()); // Teil 1
 
     betrag(arr, arr.size()); // Teil 2
-    drucke(arr, arr.size());
+
+    drucke_str(str_arr, str_arr.size()); // Teil 3
 }
