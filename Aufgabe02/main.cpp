6ea752da944d3bcbdea3d3cf32e83a31aa82842d
Daniil <pefilev007@outlook.com>
2023-11-05 17:43:05 +0100

    Funktion, die ein Array mit Zahlen fuehlt

--- Aufgabe02/main.cpp
+++ Aufgabe02/main.cpp
@@ -38,6 +38,21 @@ void drucke_str(const std::vector<std::string>& arr, const size_t& size){
     std::cout << std::endl; // \n am Ende
 }
 
+std::vector<int> lies_int(){
+    std::vector<int> arr;
+    size_t eingabe = 0; // Deffenieren Aufgabewert
+
+    std::cout << "Tippe die Zahle" << std::endl;
+
+    // Schleife, die in Array die Zahlen einsetzt
+    while (eingabe != 42)
+    {
+        std::cin >> eingabe;
+        arr.push_back(eingabe); // Setze die Zahl am Ende des Arrays ein
+    }
+    return arr;
+}
+
 int main(void){
     std::vector<int> arr{5,-4,2,-1,4};
     std::vector<std::string> str_arr{"1. Wort", "2. Wort", "3. Wort"};
@@ -45,7 +60,13 @@ int main(void){
 
     drucke_int(arr, arr.size()); // Teil 1
     std::cout << std::endl;
+
     betrag(arr, arr.size()); // Teil 2
     std::cout << std::endl;
+
     drucke_str(str_arr, str_arr.size()); // Teil 3
+    std::cout << std::endl;
+
+    std::vector<int> arr_42 = lies_int(); // Teil 4
+    drucke_int(arr_42, arr_42.size()); // Drucken Array aus Teil 4 mit der Funk aus Teil 1
 }
