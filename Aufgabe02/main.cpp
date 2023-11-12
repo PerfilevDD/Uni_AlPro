a0d5d91cd4243c5b99f3a086d79fd5b7a8623c39
Daniil <pefilev007@outlook.com>
2023-11-13 00:27:05 +0100

    1

--- Aufgabe02/main.cpp
+++ Aufgabe02/main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <vector>
 
-void drucke_int(const std::vector<int>& arr, const size_t& size){
+void drucke_int(const std::vector<int>& arr){
     /* Die Funktion drueckt die Array */
     // Sie ist const, da sie nicht veraendern wird
 
@@ -13,7 +13,7 @@ void drucke_int(const std::vector<int>& arr, const size_t& size){
     std::cout << std::endl; // \n am Ende
 }
 
-void betrag(std::vector<int>& arr, const size_t& size){
+void betrag(std::vector<int>& arr){
     /* Die Funktion drueckt die Array */
     // Arr nicht const, da sie veraendern wird
 
@@ -24,10 +24,10 @@ void betrag(std::vector<int>& arr, const size_t& size){
         }
     }
 
-    drucke_int(arr, arr.size());
+    drucke_int(arr);
 }
 
-void drucke_str(const std::vector<std::string>& arr, const size_t& size){
+void drucke_str(const std::vector<std::string>& arr){
     /* Die Funktion drueckt die Array */
     // Sie ist const, da sie nicht veraendern wird
 
@@ -49,7 +49,7 @@ std::vector<int> lies_int(){
     {
         std::cin >> eingabe;
         arr.push_back(eingabe); // Setze die Zahl am Ende des Arrays ein
-    }
+    } 
     return arr;
 }
 
@@ -58,15 +58,15 @@ int main(void){
     std::vector<std::string> str_arr{"1. Wort", "2. Wort", "3. Wort"};
 
 
-    drucke_int(arr, arr.size()); // Teil 1
+    drucke_int(arr); // Teil 1
     std::cout << std::endl;
 
-    betrag(arr, arr.size()); // Teil 2
+    betrag(arr); // Teil 2
     std::cout << std::endl;
 
-    drucke_str(str_arr, str_arr.size()); // Teil 3
+    drucke_str(str_arr); // Teil 3
     std::cout << std::endl;
 
     std::vector<int> arr_42 = lies_int(); // Teil 4
-    drucke_int(arr_42, arr_42.size()); // Drucken Array aus Teil 4 mit der Funk aus Teil 1
+    drucke_int(arr_42); // Drucken Array aus Teil 4 mit der Funk aus Teil 1
 }
