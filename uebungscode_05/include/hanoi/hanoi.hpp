8bcbf34da610d1dfde22663f140ca6f38efd469f
Daniil <pefilev007@outlook.com>
2023-11-14 23:27:16 +0100

    hanoi dateien

--- uebungscode_05/include/hanoi/hanoi.hpp
+++ uebungscode_05/include/hanoi/hanoi.hpp
@@ -3,17 +3,19 @@
 #include <vector>
 #include <cstddef>
 
-namespace hanoi{
-  
-  class Hanoi{
+namespace hanoi
+{
+
+  class Hanoi
+  {
   public:
     // Erstellt eine Startkonfiguration mit anz vielen Scheiben
     Hanoi(size_t anz);
-    
+
     // Verschiebt die oberste Scheibe (falls erlaubt) und gibt true zurück
     // genau dann wenn die Bewegung erlaubt war
     bool bewege(size_t von, size_t nach);
-    
+
     // Druckt die aktuelle Konfiguration auf der Konsole aus
     void drucke() const;
 
--- uebungscode_05/src/hanoi.cpp
+++ uebungscode_05/src/hanoi.cpp
@@ -2,11 +2,19 @@
 
 #include <iostream>
 
-namespace hanoi{
-
-Hanoi::Hanoi(size_t anz) {
-  
-  
-}
-
+namespace hanoi
+{
+    Hanoi::Hanoi(size_t anz)
+    {
+        // Die Variable stapel ist ein Array von drei Stapeln (welche wiederum Zahlenarrays sind)
+        stapel = std::vector<std::vector<size_t>>(3);
+        // Der erste Stapel soll den vollen Turm enthalten
+        stapel[0] = std::vector<size_t>(anz);
+        size_t i = anz;
+        for (auto &x : stapel[0])
+        {
+            x = i;
+            i--;
+        }
+    }
 }
