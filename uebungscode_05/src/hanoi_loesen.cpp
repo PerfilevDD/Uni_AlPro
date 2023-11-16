d61b6d2e7ce03b162302f6566785832832144572
Daniil <pefilev007@outlook.com>
2023-11-17 00:32:44 +0100

    Add: bewegung und ausdrucken

--- uebungscode_05/src/hanoi_loesen.cpp
+++ uebungscode_05/src/hanoi_loesen.cpp
@@ -1,6 +1,12 @@
 #include <hanoi/hanoi.hpp>
 #include <hanoi/hanoi_loesen.hpp>
 
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include <algorithm>
+
 namespace hanoi_loesen
 {
   Hanoi_loesen::Hanoi_loesen(size_t anz)
@@ -18,12 +24,37 @@ namespace hanoi_loesen
     }
   }
 
-  bool Hanoi_loesen::bewege_rekursiv(size_t anz, size_t von, size_t nach, size_t buf){
-    if (anz == 0){
+  void Hanoi_loesen::scheibe_in_stapel(std::vector<size_t> &stapel_von, std::vector<size_t> &stapel_nach)
+  {
+    int scheibe = stapel_von.back();
+    stapel_von.pop_back();
+    stapel_nach.push_back(scheibe);
+  }
+
+  bool Hanoi_loesen::bewege_rekursiv(size_t anz, size_t von, size_t nach, size_t buf)
+  {
+    if (anz == 0)
+    {
+      drucke();
       return true;
     }
     bewege_rekursiv(anz - 1, von, buf, nach);
+    scheibe_in_stapel(stapel[von], stapel[nach]);
     bewege_rekursiv(anz - 1, buf, nach, von);
+    return true;
   }
-
+  void Hanoi_loesen::drucke() const
+  {
+    // Print aktuelle Stapeln
+    for (auto &ein_stapel : stapel)
+    {
+      std::cout << "Stapel ";
+      for (auto &element : ein_stapel)
+      {
+        std::cout << element << " ";
+      }
+      std::cout << std::endl;
+    }
+    std::cout << std::endl;
+  }
 }
