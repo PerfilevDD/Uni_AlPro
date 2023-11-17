f0eb8d642af7b2494290f14ec598907027061cbc
Daniil <pefilev007@outlook.com>
2023-11-17 01:02:34 +0100

    Fix: drucke-Funktion Add: Anzahl von Bewegunden Del: ...

--- uebungscode_05/src/hanoi_loesen.cpp
+++ uebungscode_05/src/hanoi_loesen.cpp
@@ -15,8 +15,10 @@ namespace hanoi_loesen
     stapel = std::vector<std::vector<size_t>>(3);
     // Der erste Stapel soll den vollen Turm enthalten
     stapel[0] = std::vector<size_t>(anz);
+    zaehler = 0;
     anz_scheiben = anz;
     size_t i = anz;
+    // Addiert die Scheibe in Stapel 0
     for (auto &x : stapel[0])
     {
       x = i;
@@ -40,11 +42,14 @@ namespace hanoi_loesen
     }
     bewege_rekursiv(anz - 1, von, buf, nach);
     scheibe_in_stapel(stapel[von], stapel[nach]);
+    zaehler++;
     bewege_rekursiv(anz - 1, buf, nach, von);
     return true;
   }
   void Hanoi_loesen::drucke() const
   {
+    std::cout << "Bewegung " << zaehler << std::endl; // Anzahl der Bewegungen
+    std::cout << std::endl;
     // Print aktuelle Stapeln
     for (auto &ein_stapel : stapel)
     {
@@ -56,5 +61,29 @@ namespace hanoi_loesen
       std::cout << std::endl;
     }
     std::cout << std::endl;
+
+    for (size_t ebene = anz_scheiben; ebene > 0; --ebene) // Von oben nach unten jede Ebene durchgehen
+    {
+      for (size_t stapel_index = 0; stapel_index < 3; ++stapel_index) // Jeden Stapel durchgehen
+      {
+        if (ebene <= stapel[stapel_index].size()) // Wenn der Stapel auf dieser Ebene ein Element hat
+        {
+          size_t scheibe = stapel[stapel_index][ebene - 1];
+          std::cout << std::string(anz_scheiben - scheibe, ' '); // Leerzeichen vor der Scheibe
+          std::cout << std::string(2 * scheibe, '-');            // Die Scheibe selbst
+          std::cout << std::string(anz_scheiben - scheibe, ' '); // Leerzeichen nach der Scheibe
+        }
+        else
+        {
+          // Drucke Leerzeichen, wenn kein Element vorhanden ist
+          std::cout << std::string(2 * anz_scheiben, ' ');
+        }
+        std::cout << "   "; // Abstand zwischen den Stapeln
+      }
+      std::cout << std::endl;
+    }
+
+    // Abschließende Zeile für Stapel-Basis
+    std::cout << std::string(anz_scheiben * 6 + 10, '=') << std::endl;
   }
 }
