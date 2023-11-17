ea49469217806c646792d963d73652617d46289b
Daniil <pefilev007@outlook.com>
2023-11-17 01:01:53 +0100

    Del: Alte Funktionen

--- uebungscode_05/include/hanoi/hanoi_loesen.hpp
+++ uebungscode_05/include/hanoi/hanoi_loesen.hpp
@@ -12,10 +12,10 @@ namespace hanoi_loesen
     // Erstellt eine Startkonfiguration mit anz vielen Scheiben
     Hanoi_loesen(size_t anz);
 
-    // Verschiebt die oberste Scheibe (falls erlaubt) und gibt true zurück
-    // genau dann wenn die Bewegung erlaubt war
-    bool bewege(size_t von, size_t nach);
+    // Tauscht die Scheibe bei der Rekursion
     void scheibe_in_stapel(std::vector<size_t>& stapel_von, std::vector<size_t>& stapel_nach);
+
+    // Bewegt rekursiv die Scheibe 
     bool bewege_rekursiv(size_t anz, size_t von, size_t nach, size_t buf);
 
     // Druckt die aktuelle Konfiguration auf der Konsole aus
@@ -25,5 +25,6 @@ namespace hanoi_loesen
     // Wir haben drei Stapel. Also bietet sich ein Array von Zahlenarrays an.
     std::vector<std::vector<size_t>> stapel;
     int anz_scheiben;
+    int zaehler;
   };
 }
