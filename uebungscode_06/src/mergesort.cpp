0ab92e35868bbf728864e1f7ffabccf3b5cfc974
perfilevd0 <s74dperf@uni-bonn.de>
2023-11-19 23:10:24 +0100

    Fix: elminarr-Function

--- uebungscode_06/src/mergesort.cpp
+++ uebungscode_06/src/mergesort.cpp
@@ -1,25 +1,25 @@
 #include <mergesort.hpp>
-
+#include <iostream>
 #include <vector>
 
-std::vector<int> elminarr(const std::vector<int> &arr, const int &num)
+std::vector<int> elminarr(const std::vector<int>& arr, const int& start, const int& num)
 {
-    std::vector<int> ret_arr(num);
-    for (int i = 0; i < num - 1; i++)
+    std::vector<int> ret_arr;
+    for (int i = start; i < num; i++)
     {
         ret_arr.push_back(arr[i]);
     }
     return ret_arr;
 }
 
-std::vector<int> mergesort(std::vector<int> &A)
+std::vector<int> mergesort(std::vector<int>& A)
 {
     int k = A.size();
     if (k > 1)
     {
         int t = k / 2;
-        std::vector<int> L = elminarr(A, t);
-        std::vector<int> R = elminarr(A, k - t);
+        std::vector<int> L = elminarr(A, 0, t);
+        std::vector<int> R = elminarr(A, t, k);
         L = mergesort(L);
         R = mergesort(R);
 
@@ -31,10 +31,12 @@ std::vector<int> mergesort(std::vector<int> &A)
     }
 }
 
-std::vector<int> merge(std::vector<int> &L, std::vector<int> &R)
+std::vector<int> merge(std::vector<int>& L, std::vector<int>& R)
 {
-    std::vector<int> Y(L.size(), R.size());
-    int i, j = 0;
+    std::vector<int> Y(L.size() + R.size());
+    int i = 0;
+    int j = 0;
+    //std::cout << i << std::endl;
     while (i < L.size() && j < R.size())
     {
         if (L[i] <= R[j])
