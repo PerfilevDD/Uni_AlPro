2c4c3422ee27af85f4023223271bd77d12058ef2
perfilevd0 <s74dperf@uni-bonn.de>
2023-11-20 00:25:17 +0100

    Add: Kommentare

--- uebungscode_06/include/mergesort.hpp
+++ uebungscode_06/include/mergesort.hpp
@@ -2,8 +2,11 @@
 
 #include <vector>
 
+// Funktion, die Zahlen von einem Array in einen anderen hinzufügt
 std::vector<int> elminarr(const std::vector<int>& arr, const int& start, const int& num);
 
+// Mergesort-Funktion
 std::vector<int> mergesort(std::vector<int>& A);
 
+// Funktion, die merge 2 Arrays
 std::vector<int> merge(std::vector<int>& L, std::vector<int>& R);
