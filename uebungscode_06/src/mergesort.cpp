2573d872a48d282c842f18de7b5a25b6a90c32a7
perfilevd0 <s74dperf@uni-bonn.de>
2023-11-19 22:36:15 +0100

    Add: mergsort(not tested)

--- uebungscode_06/src/mergesort.cpp
+++ uebungscode_06/src/mergesort.cpp
@@ -1,3 +1,26 @@
 #include <mergesort.hpp>
 
-// Hier wird mergesort implementiert
+#include <vector>
+
+std::vector<int> elminarr(const std::vector<int>& arr, const int& num){
+    std::vector<int> ret_arr(num);
+    for (int i = 0; i < num - 1; i++){
+        ret_arr.push_back(arr[i]);
+    }
+    return ret_arr;
+}
+
+std::vector<int> mergesort(std::vector<int>& A){
+    int k = A.size();
+    if (k>1){
+        int t = k / 2;
+        std::vector<int> L = elminarr(A, t);
+        std::vector<int> R = elminarr(A, k - t);
+        L = mergesort(L);
+        R = mergesort(R);
+
+        return merge(L, R);
+    }
+    return A;
+}
+
