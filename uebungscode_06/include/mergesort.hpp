7b846870ba6881c5113e0efc2e12ab231d9b7e94
perfilevd0 <s74dperf@uni-bonn.de>
2023-11-19 23:09:36 +0100

    Edit: elinarr-Funktion

--- uebungscode_06/include/mergesort.hpp
+++ uebungscode_06/include/mergesort.hpp
@@ -2,6 +2,8 @@
 
 #include <vector>
 
+std::vector<int> elminarr(const std::vector<int>& arr, const int& start, const int& num);
+
 std::vector<int> mergesort(std::vector<int>& A);
 
 std::vector<int> merge(std::vector<int>& L, std::vector<int>& R);
