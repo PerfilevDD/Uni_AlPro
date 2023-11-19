4eddec0137659a937e5afe0f4a471b5057931c8b
perfilevd0 <s74dperf@uni-bonn.de>
2023-11-20 00:23:54 +0100

    Add: Funktionen um Zeit zu messen Edit: Ausgabe printet nicht mehr

--- uebungscode_06/examples/aufg2.cpp
+++ uebungscode_06/examples/aufg2.cpp
@@ -1,14 +1,40 @@
 #include <misc/zeitmesser.hpp>
 #include <misc/schnellsort.hpp>
 #include <misc/hilfsfunktionen.hpp>
+#include <mergesort.hpp>
 #include <iostream>
 #include <algorithm>
 
+int messen_schnell(const int& zahlen){
+  misc::Zeitmesser zm;
+
+  std::vector<int> bsp_zahlen(zahlen);
+  misc::fuelle_zufaellig(bsp_zahlen);
+
+  zm.start();
+  misc::schnellsort(bsp_zahlen);
+  zm.stop();
+  return zm.get_microseconds();
+}
+
+int messen_merge(const int& zahlen){
+
+  misc::Zeitmesser zm;
+
+  std::vector<int> bsp_zahlen(zahlen);
+  misc::fuelle_zufaellig(bsp_zahlen);
+
+
+  zm.start();
+  mergesort(bsp_zahlen);
+  zm.stop();
+  return zm.get_microseconds();
+
+}
 
 int main() {
-  std::vector<int> bsp_zahlen(4000);
-  misc::fuelle_zufaellig(bsp_zahlen);
-  misc::drucke_array(bsp_zahlen);
-  misc::schnellsort(bsp_zahlen);
-  misc::drucke_array(bsp_zahlen);
+  int zahlen = 100000;
+
+  std::cout << "Schnellsort: " << messen_schnell(zahlen) << std::endl;
+  std::cout << "Mergesort: " << messen_merge(zahlen) << std::endl;
 }
