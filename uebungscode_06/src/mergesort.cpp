a30ae44ca6618d385684438412770c08ac631fa8
perfilevd0 <s74dperf@uni-bonn.de>
2023-11-20 00:26:45 +0100

    Fix: In function for-loops instead while -> faster

--- uebungscode_06/src/mergesort.cpp
+++ uebungscode_06/src/mergesort.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <vector>
 
-std::vector<int> elminarr(const std::vector<int>& arr, const int& start, const int& num)
+std::vector<int> elminarr(const std::vector<int> &arr, const int &start, const int &num)
 {
     std::vector<int> ret_arr;
     for (int i = start; i < num; i++)
@@ -12,7 +12,7 @@ std::vector<int> elminarr(const std::vector<int>& arr, const int& start, const i
     return ret_arr;
 }
 
-std::vector<int> mergesort(std::vector<int>& arr)
+std::vector<int> mergesort(std::vector<int> &arr)
 {
     int arr_size = arr.size();
     if (arr_size > 1)
@@ -31,34 +31,36 @@ std::vector<int> mergesort(std::vector<int>& arr)
     }
 }
 
-std::vector<int> merge(std::vector<int>& left_arr, std::vector<int>& right_arr)
+std::vector<int> merge(std::vector<int> &left_arr, std::vector<int> &right_arr)
 {
     std::vector<int> merge_arr(left_arr.size() + right_arr.size());
     int i = 0;
     int j = 0;
-    
-    while (i < left_arr.size() && j < right_arr.size())
+
+    for (; i < left_arr.size(); i++)
     {
-        if (left_arr[i] <= right_arr[j])
+        for (; j < right_arr.size(); j++)
         {
-            merge_arr[i + j] = left_arr[i];
-            i++;
-        }
-        else
-        {
-            merge_arr[i + j] = right_arr[j];
-            j++;
+
+            if (left_arr[i] <= right_arr[j])
+            {
+                merge_arr[i + j] = left_arr[i];
+            }
+            else
+            {
+                merge_arr[i + j] = right_arr[j];
+            }
         }
     }
-    while (i < left_arr.size())
+
+    for (;i < left_arr.size(); i++)
     {
         merge_arr[i + j] = left_arr[i];
-        i++;
+        
     }
-    while (j < right_arr.size())
+    for (;j < right_arr.size(); j++)
     {
         merge_arr[i + j] = right_arr[j];
-        j++;
     }
     return merge_arr;
 }
