74245fa706fa801d3ca147ed91f656f4bfe8abd2
perfilevd0 <s74dperf@uni-bonn.de>
2023-11-20 21:16:39 +0100

    Fix: erstellung von linkem und rechtem Array Add: Kommentare

--- uebungscode_06/src/mergesort.cpp
+++ uebungscode_06/src/mergesort.cpp
@@ -2,9 +2,13 @@
 #include <iostream>
 #include <vector>
 
-std::vector<int> elminarr(const std::vector<int>& arr, const int& start, const int& num)
+std::vector<int> elminarr(const std::vector<int> &arr, const int &start, const int &num)
 {
+    // Funktion, die ein Array erstellt.
+    // Wird benutzt, um linkes und rechtes Array zu erstellen
+
     std::vector<int> ret_arr;
+    
     for (int i = start; i < num; i++)
     {
         ret_arr.push_back(arr[i]);
@@ -12,17 +16,25 @@ std::vector<int> elminarr(const std::vector<int>& arr, const int& start, const i
     return ret_arr;
 }
 
-std::vector<int> mergesort(std::vector<int>& arr)
+std::vector<int> mergesort(std::vector<int> &arr)
 {
+    // Mergesort-Funktion teile das gegebene Array in zwei anderen und ruft sich rekursiv
     int arr_size = arr.size();
+
     if (arr_size > 1)
     {
         int middle = arr_size / 2;
-        std::vector<int> left_arr = elminarr(arr, 0, arr_size);
-        std::vector<int> right_arr = elminarr(arr, arr_size, middle);
+
+        // Ertstellen ein Array, das alle Zeilen ursp. Array bis zur Mitte hat
+        std::vector<int> left_arr = elminarr(arr, 0, middle);
+        // Ertstellen ein Array, das alle Zeilen ursp. Array nach der Mitte hat
+        std::vector<int> right_arr = elminarr(arr, middle, arr_size);
+
+        // Rufen rekursive Funktionen
         left_arr = mergesort(left_arr);
         right_arr = mergesort(right_arr);
 
+        // Rufen merge-Funktion
         return merge(left_arr, right_arr);
     }
     else
@@ -31,13 +43,13 @@ std::vector<int> mergesort(std::vector<int>& arr)
     }
 }
 
-std::vector<int> merge(std::vector<int>& left_arr, std::vector<int>& right_arr)
+std::vector<int> merge(std::vector<int> &left_arr, std::vector<int> &right_arr)
 {
     std::vector<int> merge_arr(left_arr.size() + right_arr.size());
     int i = 0;
     int j = 0;
-    
-    
+
+    // Schleife, um das Array mit Zahlen von den anderen Arrays zu fuellen
     while (i < left_arr.size() && j < right_arr.size())
     {
         if (left_arr[i] <= right_arr[j])
@@ -51,6 +63,8 @@ std::vector<int> merge(std::vector<int>& left_arr, std::vector<int>& right_arr)
             j++;
         }
     }
+
+    // Zwei weiter Schleifen, die die Zahl am Ende des Arrays hinzufuegen
     while (i < left_arr.size())
     {
         merge_arr[i + j] = left_arr[i];
