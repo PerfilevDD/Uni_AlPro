cb64a0ed3a9cedf871b1d8ac5b4f052765da600a
perfilevd0 <s74dperf@uni-bonn.de>
2023-11-21 16:53:20 +0100

    Add: Funktionen um Knoten zu erstellen bzw. loeschen Edit: vector aus privat -> public

--- uebungscode_06/include/simpler_graph/simpler_graph.hpp
+++ uebungscode_06/include/simpler_graph/simpler_graph.hpp
@@ -3,15 +3,23 @@
 #include <vector>
 #include <cstddef>
 
-namespace SimplerGraph{
-  
-  class SimplerGraph{
+namespace SimplerGraph
+{
+
+  class SimplerGraph
+  {
   public:
     // Erstellt einen Graph mit einer festen Knotenanzahl
     SimplerGraph(size_t knoten_anzahl);
-    
+
+    void drucken(); // Druck funktion
+
+    // Funktionen um die Knoten zu erstellen bzw. zu loeschen
+    void new_knoten(const size_t &num1, const size_t &num2);
+    void enf_knoten(const size_t &num1, const size_t &num2);
 
   private:
+    std::vector<std::vector<bool>> matrix;
     const size_t knoten_anz;
   };
 }
