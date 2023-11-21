3634c78ccb036ffc90eed6da1ccb9099ef90bc58
perfilevd0 <s74dperf@uni-bonn.de>
2023-11-21 16:46:40 +0100

    Add: drucken die Zeit alle Sort. Alg

--- uebungscode_06/examples/aufg2.cpp
+++ uebungscode_06/examples/aufg2.cpp
@@ -6,7 +6,8 @@
 #include <iostream>
 #include <algorithm>
 
-int messen_schnell(const int& zahlen){
+int messen_schnell(const int &zahlen)
+{
   misc::Zeitmesser zm;
 
   std::vector<int> bsp_zahlen(zahlen);
@@ -18,39 +19,63 @@ int messen_schnell(const int& zahlen){
   return zm.get_microseconds();
 }
 
-int messen_merge(const int& zahlen){
+int messen_merge(const int &zahlen)
+{
 
   misc::Zeitmesser zm;
 
   std::vector<int> bsp_zahlen(zahlen);
   misc::fuelle_zufaellig(bsp_zahlen);
-
 
   zm.start();
   mergesort(bsp_zahlen);
   zm.stop();
   return zm.get_microseconds();
-
 }
 
-int minsort(const int& zahlen){
+int messen_minsort(const int &zahlen)
+{
 
   misc::Zeitmesser zm;
 
   std::vector<int> bsp_zahlen(zahlen);
   misc::fuelle_zufaellig(bsp_zahlen);
 
-
   zm.start();
   minsort(bsp_zahlen);
   zm.stop();
   return zm.get_microseconds();
-
 }
 
-int main() {
-  int zahlen = 100000;
+void druecken(std::vector<int>& arr){
+  for (const int zahl : arr){
+    std::cout << zahl << " " << std::endl;
+  }
+}
 
-  std::cout << "Schnellsort: " << messen_schnell(zahlen) << std::endl;
-  std::cout << "Mergesort: " << messen_merge(zahlen) << std::endl;
+int main()
+{
+  int zahlen = 100000;
+  int schritt = 10;
+  std::vector<int> schnell_zeiten, merge_zeiten, minsort_zeiten, alle_zahlen;
+
+  for (int i = 10; i <= zahlen; i = i * schritt)
+  {
+    schnell_zeiten.push_back(messen_schnell(i));
+    merge_zeiten.push_back(messen_merge(i));
+    minsort_zeiten.push_back(messen_minsort(i));
+    alle_zahlen.push_back(i);
+  }
+
+  std::cout << "Schnellsort: " << std::endl;
+  druecken(schnell_zeiten);
+
+  std::cout << "Mergesort: " << std::endl;
+  druecken(merge_zeiten);
+
+  std::cout << "Minsort: " << std::endl;
+  druecken(minsort_zeiten);
+
+  std::cout << "Alle Zahlen: " << std::endl;
+  druecken(alle_zahlen);
 }
