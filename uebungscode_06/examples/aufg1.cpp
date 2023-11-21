e116fcf07a290133990c4d5e04881c0a6b8cc60e
perfilevd0 <s74dperf@uni-bonn.de>
2023-11-21 16:49:59 +0100

    Add: Kommentare

--- uebungscode_06/examples/aufg1.cpp
+++ uebungscode_06/examples/aufg1.cpp
@@ -4,10 +4,12 @@
 #include <iostream>
 
 int main() {
+  // Erstellen ein Array und fuegen ran. Zahlen hin
   std::vector<int> bsp_zahlen(10);
   misc::fuelle_zufaellig(bsp_zahlen);
   misc::drucke_array(bsp_zahlen);
   
+  // Erstellen ein sortiertes Array
   std::vector<int> merg_sorted = mergesort(bsp_zahlen);
   misc::drucke_array(merg_sorted);
 }
