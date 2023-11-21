152653da0282c053b2e9f07a73fdddf71b042253
perfilevd0 <s74dperf@uni-bonn.de>
2023-11-21 17:04:18 +0100

    Add: Beispiele Add: Kommentare

--- uebungscode_06/examples/aufg3.cpp
+++ uebungscode_06/examples/aufg3.cpp
@@ -2,7 +2,22 @@
 
 #include <iostream>
 
+int main()
+{
+  SimplerGraph::SimplerGraph sg(3); // Inizialisieren Graphen und geben die Anzahl der Knoten
 
-int main() {
-  // ...
+  sg.new_knoten(0,1); // Erstellen eine Verbindung zwischen den Knoten
+  sg.drucken(); // Drucken die Knoten mit der Verbindung
+
+  sg.enf_knoten(0,1); // Entfernen die Verbindung zwischen den Knoten
+
+  // Illegal
+  sg.new_knoten(2,3); // Erstellen eine Verbindung zwischen den Knoten
+
+  sg.new_knoten(1,1); // Erstellen eine Verbindung zwischen den Knoten
+  sg.new_knoten(2,1); // Erstellen eine Verbindung zwischen den Knoten
+  sg.new_knoten(1,0); // Erstellen eine Verbindung zwischen den Knoten
+
+  sg.drucken(); // Drucken die Knoten mit der Verbindung
+
 }
