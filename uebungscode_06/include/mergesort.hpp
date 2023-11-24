8e852275e6899878b764ddc7524acf7f3af5dfb7
Ilya <ilya.kohn@icloud.com>
2023-11-24 09:28:28 +0100

    Kommentare für Header

--- uebungscode_06/include/mergesort.hpp
+++ uebungscode_06/include/mergesort.hpp
@@ -3,5 +3,6 @@
 
 // Hier kommt die Deklaration (und Beschreibung) von mergesort hin
 
+// Deklaration von mergesort und merge die nach dem Divide and Conquer Prinzip funktionieren
 void mergesort(std::vector<int>&);
 void merge(std::vector<int>&,std::vector<int>&,std::vector<int>&);
