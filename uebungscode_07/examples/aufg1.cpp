14515ed4de0c6e4e331da2524457f4b4631864fb
perfilevd0 <s74dperf@uni-bonn.de>
2023-11-26 00:22:19 +0100

    Add: Code aus Uebungsblatt 06

--- uebungscode_07/examples/aufg1.cpp
+++ uebungscode_07/examples/aufg1.cpp
@@ -1,13 +1,30 @@
 #include <simpler_graph/simpler_graph.hpp>
 
-#define FMT_HEADER_ONLY  // Dieses Makro ist nötig, um die Bibliothek simpel einzubinden
-#include <fmt/core.h>    // Muss eingebunden werden um {fmt} zu verwenden
-#include <fmt/ranges.h>  // Muss zur Ausgabe von Arrays eingebunden werden
-#include <vector>        // Stellt dynamische Arrays zur Verfügung
+#define FMT_HEADER_ONLY // Dieses Makro ist nötig, um die Bibliothek simpel einzubinden
+#include <fmt/core.h>   // Muss eingebunden werden um {fmt} zu verwenden
+#include <fmt/ranges.h> // Muss zur Ausgabe von Arrays eingebunden werden
+#include <vector>       // Stellt dynamische Arrays zur Verfügung
 
-int main() {
+int main()
+{
   // ...
-  
+
+  SimplerGraph::SimplerGraph sg(3); // Inizialisieren Graphen und geben die Anzahl der Knoten
+
+  sg.new_knoten(0, 1); // Erstellen eine Verbindung zwischen den Knoten
+  sg.drucken();        // Drucken die Knoten mit der Verbindung
+
+  sg.enf_knoten(0, 1); // Entfernen die Verbindung zwischen den Knoten
+
+  // Illegal
+  sg.new_knoten(2, 3); // Erstellen eine Verbindung zwischen den Knoten
+
+  sg.new_knoten(1, 1); // Erstellen eine Verbindung zwischen den Knoten
+  sg.new_knoten(2, 1); // Erstellen eine Verbindung zwischen den Knoten
+  sg.new_knoten(1, 0); // Erstellen eine Verbindung zwischen den Knoten
+
+  sg.drucken(); // Drucken die Knoten mit der Verbindung
+
   // Nutzen Sie die {fmt}-Library um das Array auszugeben
   fmt::print("HIER DAS ARRAY WIE AUSGEBEN\n");
 }
--- uebungscode_07/src/simpler_graph.cpp
+++ uebungscode_07/src/simpler_graph.cpp
@@ -1,3 +1,44 @@
 #include <simpler_graph/simpler_graph.hpp>
+#include <iostream>
+#include <vector>
 
-// Ihre Implementierung
+namespace SimplerGraph
+{
+  SimplerGraph::SimplerGraph(size_t knoten_anzahl) : knoten_anz(knoten_anzahl),
+                                                     matrix(knoten_anzahl, std::vector<bool>(knoten_anzahl, false))
+  {
+    // Erstellen eine Klasse mit gegebener Zahl der Anzahl der Knoten
+  }
+
+  void SimplerGraph::new_knoten(const size_t &num1, const size_t &num2)
+  {
+    // Erstellen neue Knoten
+    if (num1 >= 0 && num1 < knoten_anz && num2 >= 0 && num2 < knoten_anz)
+    {
+      matrix[num1][num2] = true;
+    }
+  }
+  void SimplerGraph::enf_knoten(const size_t &num1, const size_t &num2)
+  {
+    // Entwernen die Knote
+    if (num1 >= 0 && num1 < knoten_anz && num2 >= 0 && num2 < knoten_anz)
+    {
+      matrix[num1][num2] = false;
+    }
+  }
+
+  void SimplerGraph::drucken()
+  {
+    // Drucken alle exestierende Knoten mit ensprechender Verbindung
+
+    // Gehen Array durch
+    for (int i = 0; i < knoten_anz; i++) {
+      for (int j = 0; j < knoten_anz; j++) {
+        if (matrix[i][j]==true) {
+          // Falls eine Verbindung zwischen den Knoten exestiert, druecken sie aus
+          std::cout << "(" << i << " -> " << j << ")" << std::endl;
+        }
+      }
+    }
+  }
+}
