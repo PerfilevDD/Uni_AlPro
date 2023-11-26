0381333d3943e0b3634806c7bf2d340a4e5ef63c
perfilevd0 <s74dperf@uni-bonn.de>
2023-11-26 23:12:29 +0100

    Fix: Pruefe des Startsknotes in Breitensuche Add: Tiefensuche(leer) Edit: Kommentare Edit: Formatierung des Codes

--- uebungscode_07/src/simpler_graph.cpp
+++ uebungscode_07/src/simpler_graph.cpp
@@ -1,89 +1,83 @@
-#include <simpler_graph/simpler_graph.hpp>
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <simpler_graph/simpler_graph.hpp>
+#include <vector>
 
-namespace SimplerGraph
-{
+namespace SimplerGraph {
     SimplerGraph::SimplerGraph(size_t knoten_anzahl) : knoten_anz(knoten_anzahl),
-                                                       matrix(knoten_anzahl, std::vector<bool>(knoten_anzahl, false))
-    {
+                                                       matrix(knoten_anzahl, std::vector<bool>(knoten_anzahl, false)) {
         // Erstellen eine Klasse mit gegebener Zahl der Anzahl der Knoten
     }
 
-    void SimplerGraph::new_knoten(const size_t &num1, const size_t &num2)
-    {
+    void SimplerGraph::neue_kante(const size_t &num1, const size_t &num2) {
         // Erstellen neue Knoten
         if (num1 >= 0 && num1 < knoten_anz && num2 >= 0 && num2 < knoten_anz)
+
         {
             matrix[num1][num2] = true;
         }
     }
-    void SimplerGraph::enf_knoten(const size_t &num1, const size_t &num2)
-    {
+    void SimplerGraph::enf_kante(const size_t &num1, const size_t &num2) {
         // Entwernen die Knote
-        if (num1 >= 0 && num1 < knoten_anz && num2 >= 0 && num2 < knoten_anz)
-        {
+        if (num1 >= 0 && num1 < knoten_anz && num2 >= 0 && num2 < knoten_anz) {
             matrix[num1][num2] = false;
         }
     }
 
-    bool SimplerGraph::check_knoten(std::vector<size_t> &arr, const size_t &j)
-    {
-        for (const auto &unerk_knoten : arr)
-        {
-            if (unerk_knoten == j)
-            {
+    bool SimplerGraph::check_knoten(std::vector<size_t> &arr, const size_t &j) {
+        // Funktion prueft ob ein Knoten im Array schon ist
+        for (const auto &unerk_knoten : arr) {
+            if (unerk_knoten == j) {
                 return true;
             }
         }
         return false;
     }
 
-    void SimplerGraph::BreitenSuche(const size_t &s)
-    {
-        // TODO: Überprufen auf s; kommentare; print
+    void SimplerGraph::BreitenSuche(const size_t &s) {
         std::queue<size_t> queue;
-        queue.push(s);
-        while (!queue.empty())
-        {
-            int v = queue.front();
-            queue.pop();
-            for (size_t j = 0; j < knoten_anz; j++)
-            {
-                if (matrix[v][j])
-                {
-                    // Prufen, damit nur unterer Teil der Graph ausgedruckt wuerde
-                    if (j > s)
-                    {
-                        if (!check_knoten(folge, j))
-                        {
-                            std::cout << j << std::endl;
-                            folge.push_back(j);
-                            queue.push(j);
+
+        // Pruefen falls der Startknoten exestiert im Array aller Knoten
+        if (s < knoten_anz) {
+            queue.push(s);
+            // Gehen Queue durch
+            while (!queue.empty()) {
+                int v = queue.front();
+                queue.pop();
+                for (size_t j = 0; j < knoten_anz; j++) {
+                    // Pruefen falls eine Kante exestiert
+                    if (matrix[v][j]) {
+                        // Prufen, damit nur unterer Teil der Graph ausgedruckt wuerde
+                        if (j > s) {
+                            // Pruefen falls der Knoten schon besucht war
+                            if (!check_knoten(folge, j)) {
+                                folge.push_back(j);
+                                queue.push(j);
+                            }
                         }
                     }
                 }
             }
         }
-        for (const auto &knoten : folge)
-        {
+        // DEL
+        for (const auto &knoten : folge) {
             std::cout << knoten << " ";
         }
         std::cout << std::endl;
     }
 
-    void SimplerGraph::drucken()
-    {
+
+    void SimplerGraph::TiefenSuche(const size_t &s){
+
+    }
+
+    void SimplerGraph::drucken() {
         // Drucken alle exestierende Knoten mit ensprechender Verbindung
 
         // Gehen Array durch
-        for (int i = 0; i < knoten_anz; i++)
-        {
-            for (int j = 0; j < knoten_anz; j++)
-            {
-                if (matrix[i][j] == true)
-                {
+        for (int i = 0; i < knoten_anz; i++) {
+            for (int j = 0; j < knoten_anz; j++) {
+                if (matrix[i][j] == true) {
                     // Falls eine Verbindung zwischen den Knoten exestiert, druecken sie aus
                     std::cout << "(" << i << " -> " << j << ")" << std::endl;
                 }
