367bb7b89d2e70ce5a65326e78e6863698e77a69
perfilevd0 <s74dperf@uni-bonn.de>
2023-11-26 01:36:51 +0100

    BreitenSuche Indexfinden-Funktion Vector der Knoten

--- uebungscode_07/include/simpler_graph/simpler_graph.hpp
+++ uebungscode_07/include/simpler_graph/simpler_graph.hpp
@@ -6,20 +6,24 @@
 namespace SimplerGraph
 {
 
-  class SimplerGraph
-  {
-  public:
-    // Erstellt einen Graph mit einer festen Knotenanzahl
-    SimplerGraph(size_t knoten_anzahl);
+    class SimplerGraph
+    {
+    public:
+        // Erstellt einen Graph mit einer festen Knotenanzahl
+        SimplerGraph(size_t knoten_anzahl);
 
-    void drucken(); // Druck funktion
+        void drucken(); // Druck funktion
 
-    // Funktionen um die Knoten zu erstellen bzw. zu loeschen
-    void new_knoten(const size_t &num1, const size_t &num2);
-    void enf_knoten(const size_t &num1, const size_t &num2);
+        void BreitenSuche(const size_t &s);
 
-  private:
-    std::vector<std::vector<bool>> matrix;
-    const size_t knoten_anz;
-  };
+        // Funktionen um die Knoten zu erstellen bzw. zu loeschen
+        void new_knoten(const size_t &num1, const size_t &num2);
+        void enf_knoten(const size_t &num1, const size_t &num2);
+        std::vector<size_t> folge;
+
+    private:
+        bool check_knoten(std::vector<size_t> &arr, const size_t &j);
+        std::vector<std::vector<bool>> matrix;
+        const size_t knoten_anz;
+    };
 }
