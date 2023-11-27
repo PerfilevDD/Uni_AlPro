fe64f5bed5a32927eb58fe3d63a5d903db1e0bad
perfilevd0 <s74dperf@uni-bonn.de>
2023-11-27 07:42:35 +0100

    Edit: Check fuer gerichtete Graph

--- uebungscode_07/examples/aufg1.cpp
+++ uebungscode_07/examples/aufg1.cpp
@@ -1,29 +1,30 @@
 #include <simpler_graph/simpler_graph.hpp>
 
-#define FMT_HEADER_ONLY // Dieses Makro ist nötig, um die Bibliothek simpel einzubinden
-#include <fmt/core.h>   // Muss eingebunden werden um {fmt} zu verwenden
-#include <fmt/ranges.h> // Muss zur Ausgabe von Arrays eingebunden werden
-#include <vector>       // Stellt dynamische Arrays zur Verfügung
+#define FMT_HEADER_ONLY  // Dieses Makro ist nötig, um die Bibliothek simpel einzubinden
+#include <fmt/core.h>    // Muss eingebunden werden um {fmt} zu verwenden
+#include <fmt/ranges.h>  // Muss zur Ausgabe von Arrays eingebunden werden
 
-int main()
-{
-  // ...
+#include <vector>  // Stellt dynamische Arrays zur Verfügung
 
-  SimplerGraph::SimplerGraph sg(4); // Inizialisieren Graphen und geben die Anzahl der Knoten
+int main() {
+    // ...
 
-  sg.new_knoten(0, 1); // Erstellen eine Verbindung zwischen den Knoten
-  sg.new_knoten(1, 2); // Erstellen eine Verbindung zwischen den Knoten
-  sg.new_knoten(1, 3); // Erstellen eine Verbindung zwischen den Knoten
+    SimplerGraph::SimplerGraph sg(4);  // Inizialisieren Graphen und geben die Anzahl der Knoten
 
+    bool is_gerichtet = true;
 
-  /*sg.new_knoten(1, 0); // Erstellen eine Verbindung zwischen den Knoten
-  sg.new_knoten(2, 1); // Erstellen eine Verbindung zwischen den Knoten
-  sg.new_knoten(3, 1); // Erstellen eine Verbindung zwischen den Knoten*/
+    sg.neue_kante(0, 1, is_gerichtet);  // Erstellen eine Verbindung zwischen den Knoten
+    sg.neue_kante(1, 2, is_gerichtet);  // Erstellen eine Verbindung zwischen den Knoten
+    sg.neue_kante(1, 3, is_gerichtet);                // Erstellen eine Verbindung zwischen den Knoten
 
-  sg.BreitenSuche(0);
+    /*sg.new_knoten(1, 0); // Erstellen eine Verbindung zwischen den Knoten
+    sg.new_knoten(2, 1); // Erstellen eine Verbindung zwischen den Knoten
+    sg.new_knoten(3, 1); // Erstellen eine Verbindung zwischen den Knoten*/
 
-  sg.drucken(); // Drucken die Knoten mit der Verbindung
+    sg.BreitenSuche(0);
 
-  // Nutzen Sie die {fmt}-Library um das Array auszugeben
-  fmt::print("HIER DAS ARRAY WIE AUSGEBEN\n");
+    sg.drucken();  // Drucken die Knoten mit der Verbindung
+
+    // Nutzen Sie die {fmt}-Library um das Array auszugeben
+    fmt::print("HIER DAS ARRAY WIE AUSGEBEN\n");
 }
--- uebungscode_07/examples/aufg2.cpp
+++ uebungscode_07/examples/aufg2.cpp
@@ -3,11 +3,35 @@
 #define FMT_HEADER_ONLY  // Dieses Makro ist nötig, um die Bibliothek simpel einzubinden
 #include <fmt/core.h>    // Muss eingebunden werden um {fmt} zu verwenden
 #include <fmt/ranges.h>  // Muss zur Ausgabe von Arrays eingebunden werden
-#include <vector>        // Stellt dynamische Arrays zur Verfügung
+#include <iostream>
+
+#include <vector>  // Stellt dynamische Arrays zur Verfügung
 
 int main() {
-  // ...
-  
-  // Nutzen Sie die {fmt}-Library um das Array auszugeben
-  fmt::print("HIER DAS ARRAY WIE AUSGEBEN\n");
+    // ...
+
+    // ...
+
+    SimplerGraph::SimplerGraph sg(4);  // Inizialisieren Graphen und geben die Anzahl der Knoten
+
+    bool is_gerichtet = false;
+
+    sg.neue_kante(0, 1, is_gerichtet);  // Erstellen eine Verbindung zwischen den Knoten
+    sg.neue_kante(1, 2, is_gerichtet);  // Erstellen eine Verbindung zwischen den Knoten
+    sg.neue_kante(1, 3, is_gerichtet);  // Erstellen eine Verbindung zwischen den Knoten
+
+    /*sg.new_knoten(1, 0); // Erstellen eine Verbindung zwischen den Knoten
+    sg.new_knoten(2, 1); // Erstellen eine Verbindung zwischen den Knoten
+    sg.new_knoten(3, 1); // Erstellen eine Verbindung zwischen den Knoten*/
+
+    sg.TiefenSuche(0);
+    // DEL
+    for (const auto &knoten : sg.folge) {
+        std::cout << knoten << " ";
+    }
+    std::cout << std::endl;
+
+    sg.drucken();  // Drucken die Knoten mit der Verbindung
+
+    fmt::print("HIER DAS ARRAY WIE AUSGEBEN\n");
 }
