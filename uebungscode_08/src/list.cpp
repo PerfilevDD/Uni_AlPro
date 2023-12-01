d6eb5d4827b76ac865df51288e7728ffe8defec7
perfilevd0 <s74dperf@uni-bonn.de>
2023-12-01 01:45:03 +0100

    Add: print funktion      insert funktion

--- uebungscode_08/src/list.cpp
+++ uebungscode_08/src/list.cpp
@@ -1,18 +1,30 @@
-#include <datenstrukturen/listnode.hpp>
 #include <datenstrukturen/list.hpp>
+#include <datenstrukturen/listnode.hpp>
+#include <iostream>
+#include <memory>
 
 namespace Datenstrukturen {
-    ListNode::ListNode(int data) : data_(data){
+    ListNode::ListNode(int data) : data_(data) {
         // Erstellt ein Node mit eingegebener Zahl
     }
-    List::List() : head(nullptr){
+    List::List() : head(nullptr) {
         // Initializieren Nullpointer
     }
-    ListNodeptr List::insert_front(const int x){
-
+    ListNodeptr List::insert_front(const int x) {
+        ListNodeptr n = std::make_shared<ListNode>(x);
+        n->next = head;
+        head = n;
+        return head;
     }
 
-    void print(){
-        
+    void List::print() const {
+        ListNodeptr aktuell = head;
+        while (aktuell) {
+            std::cout << "ss" << std::endl;
+            std::cout << aktuell->data_;
+            aktuell = aktuell->next;
+        }
+        std::cout << head << std::endl;
+        // std::cout << "ss" << std::endl;
     }
-}
+}  // namespace Datenstrukturen
