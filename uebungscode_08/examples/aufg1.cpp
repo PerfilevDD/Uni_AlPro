ec43e2ae8c1c40414a80c990e79c1ed80478a482
perfilevd0 <s74dperf@uni-bonn.de>
2023-12-01 02:22:12 +0100

    Beispiele

--- uebungscode_08/examples/aufg1.cpp
+++ uebungscode_08/examples/aufg1.cpp
@@ -1,6 +1,12 @@
 #include <datenstrukturen/list.hpp>
+#include <iostream>
 
 int main() {
-  // ...
-  
+  Datenstrukturen::List list;
+  list.insert_front(2);
+  list.insert_front(3);
+  list.print();
+  std::cout << "=========" <<std::endl;
+  list.remove_front();
+  list.print();
 }
