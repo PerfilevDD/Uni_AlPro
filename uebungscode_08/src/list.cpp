395745f2d6a554adfc539fb578072f16f1c8326e
perfilevd0 <s74dperf@uni-bonn.de>
2023-12-01 01:16:45 +0100

    Add: Erstellen namespace      Erste Funktionen aus der Headerdatei

---
+++ uebungscode_08/src/list.cpp
@@ -0,0 +1,18 @@
+#include <datenstrukturen/listnode.hpp>
+#include <datenstrukturen/list.hpp>
+
+namespace Datenstrukturen {
+    ListNode::ListNode(int data) : data_(data){
+        // Erstellt ein Node mit eingegebener Zahl
+    }
+    List::List() : head(nullptr){
+        // Initializieren Nullpointer
+    }
+    ListNodeptr List::insert_front(const int x){
+
+    }
+
+    void print(){
+        
+    }
+}
