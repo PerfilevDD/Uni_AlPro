c733237262ede1b3f49fa9a157aa57aa1b22c254
perfilevd0 <s74dperf@uni-bonn.de>
2023-12-01 02:05:02 +0100

    Bugs fixed

--- uebungscode_08/src/list.cpp
+++ uebungscode_08/src/list.cpp
@@ -4,7 +4,7 @@
 #include <memory>
 
 namespace Datenstrukturen {
-    ListNode::ListNode(int data) : data_(data) {
+    ListNode::ListNode(int data) : data_(data), next(nullptr) {
         // Erstellt ein Node mit eingegebener Zahl
     }
     List::List() : head(nullptr) {
@@ -21,9 +21,10 @@ namespace Datenstrukturen {
         ListNodeptr aktuell = head;
         while (aktuell) {
             std::cout << "ss" << std::endl;
-            std::cout << aktuell->data_;
+            std::cout << aktuell->data_ << "->";
             aktuell = aktuell->next;
         }
+        std::cout << "null" << std::endl;
         std::cout << head << std::endl;
         // std::cout << "ss" << std::endl;
     }
