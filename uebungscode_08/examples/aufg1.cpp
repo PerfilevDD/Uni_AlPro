826afeefb64bd2fe6bd9410a62d7741e1b89b34d
perfilevd0 <s74dperf@uni-bonn.de>
2023-12-03 01:17:32 +0100

    Add: Erstellt Funktionen fuer insert_after und front      Neue Kommentare

--- uebungscode_08/examples/aufg1.cpp
+++ uebungscode_08/examples/aufg1.cpp
@@ -3,15 +3,17 @@
 #include <memory>
 
 int main() {
-  Datenstrukturen::List list;
-  list.insert_front(2);
-  //list.insert_front(3);
-  list.print();
-  std::cout << "=========" <<std::endl;
+    Datenstrukturen::List list;
+    auto insert_1 = list.insert_front(1);  // Fuegt ein Element am Anfang der Liste ninzu
+    auto insert_4 = list.insert_front(4);
+    list.print();  // Druck die Liste
 
+    std::cout << "=========" << std::endl;
 
-  
-  list.insert_after(nullptr, 2);
-
-  list.print();
+    auto insert_2 = list.insert_after(insert_4, 2);  // Fuegt ein Element nach einem Element der Liste ninzu
+    auto insert_3 = list.insert_after(insert_4, 3);
+    auto insert_5 = list.insert_front(5);
+    auto insert_6 = list.insert_after(insert_3, 6);
+    list.remove_front();  // Entfernt ein Element am Anfang der Liste
+    list.print();
 }
