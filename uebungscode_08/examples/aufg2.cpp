f6d10c6e12f9d4e3807a0a43de872b479ab52eaf
perfilevd0 <s74dperf@uni-bonn.de>
2023-12-04 00:37:44 +0100

    Add: Init von Tree und left child

--- uebungscode_08/examples/aufg2.cpp
+++ uebungscode_08/examples/aufg2.cpp
@@ -1,9 +1,13 @@
 #include <datenstrukturen/binarytree.hpp>
+#include <iostream>
 
 int main() {
-  Datenstrukturen::BinaryTree binaryTree(2);
-  
-  
+    Datenstrukturen::BinaryTree binaryTree(2);
+    auto root = binaryTree.get_root();
 
-
+    std::cout << "main " << root << std::endl;
+    root->set_left_child(3);
+    auto left1 = root->get_left_child();
+    left1->set_left_child(5);
+    binaryTree.print();
 }
