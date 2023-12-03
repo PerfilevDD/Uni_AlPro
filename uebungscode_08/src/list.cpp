39d2c17fdf9bc41c67e4e2e2dace12321c272b98
perfilevd0 <s74dperf@uni-bonn.de>
2023-12-03 01:18:45 +0100

    Add: insert_front Funktion      neue Kommentare Edit: print Funktion

--- uebungscode_08/src/list.cpp
+++ uebungscode_08/src/list.cpp
@@ -8,20 +8,21 @@ namespace Datenstrukturen {
         // Erstellt ein Node mit eingegebener Zahl
     }
     List::List() : head(nullptr) {
-        // Initializieren Nullpointer
+        // Initializiert head
     }
     ListNodeptr List::insert_front(const int x) {
         ListNodeptr n = std::make_shared<ListNode>(x);  // Erstellen neu Knoten
-        n->next = head; // Setzen pointer von n auf head
-        head = n; // n ist jetzt head
-        std::cout << std::endl;
-
+        n->next = head;                                 // Setzen pointer von n auf head
+        head = n;                                       // n ist jetzt head
         return head;
     }
-    
-    ListNodeptr List::insert_after(const ListNodeptr& pred, const int x){
-        std::cout << "insert after  " << pred << std::endl;
-        return head;
+
+    ListNodeptr List::insert_after(const ListNodeptr& pred, const int x) {
+        ListNodeptr n = std::make_shared<ListNode>(x);  // Erstellen neu Knoten
+        ListNodeptr alt_pointer = pred->next;           // Alter Pointer, der auf den naechsten Element nach pred zeigte
+        pred->next = n;                                 // Setzen Pointer von altem Element auf neuem Pointer
+        n->next = alt_pointer;                          // Setzen naechster Pointer nach n auf vorherigen naechsten Poninter
+        return n;
     }
 
     ListNodeptr List::remove_front() {
@@ -35,13 +36,13 @@ namespace Datenstrukturen {
     }
 
     void List::print() const {
+        // Funktuion drueckt die Liste
         ListNodeptr aktuell = head;
+        // Gehen jeder Element der Liste durch, um alle auszudruecken
         while (aktuell) {
             std::cout << aktuell->data_ << "->";
             aktuell = aktuell->next;
         }
         std::cout << "null" << std::endl;
-        std::cout << head << std::endl;
-        // std::cout << "ss" << std::endl;
     }
 }  // namespace Datenstrukturen
