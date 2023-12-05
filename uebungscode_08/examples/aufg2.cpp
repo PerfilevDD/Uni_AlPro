4b577e943a27c36fbae84aa8fa7342fe378d72f4
perfilevd0 <s74dperf@uni-bonn.de>
2023-12-05 18:41:14 +0100

    Add: Kommentare      Verbessert die Beispiele

--- uebungscode_08/examples/aufg2.cpp
+++ uebungscode_08/examples/aufg2.cpp
@@ -2,12 +2,59 @@
 #include <iostream>
 
 int main() {
-    Datenstrukturen::BinaryTree binaryTree(2);
-    auto root = binaryTree.get_root();
+    Datenstrukturen::BinaryTree binaryTree(42);
+    auto root = binaryTree.get_root();  // Init root
 
-    std::cout << "main " << root << std::endl;
-    root->set_left_child(3);
-    auto left1 = root->get_left_child();
-    left1->set_left_child(5);
-    binaryTree.print();
+    // Erstellen Knoten aus der Aufgabe
+
+    // Links von root
+    
+        // 11
+        root->set_left_child(11);
+        auto left11 = root->get_left_child();
+
+        // 9
+        left11->set_left_child(9);
+        auto left9 = left11->get_left_child();
+
+        // 0
+        left11->set_right_child(0);
+        auto right0 = left11->get_right_child();
+
+        // -6
+        left9->set_left_child(-6);
+        auto leftm6 = left9->get_left_child();
+
+        // 22
+        left9->set_right_child(22);
+        auto right22 = left9->get_right_child();
+    
+
+    // Rechts von root
+    
+        // r22
+        root->set_right_child(22);
+        auto rightr22 = root->get_right_child();
+
+        // 544
+        rightr22->set_right_child(544);
+        auto right544 = rightr22->get_right_child();
+
+        // r11
+        right544->set_left_child(11);
+        auto leftr11 = right544->get_left_child();
+    
+
+    binaryTree.print();  // Print Tree
+
+    std::cout << "===========" << std::endl;
+
+    root->remove_right_child();  // Delete right child von root
+    binaryTree.print();          // Print Tree
+
+    std::cout << "===========" << std::endl;
+
+    // Delete left child von 11(links oben)
+    left11->remove_left_child();
+    binaryTree.print();  // Print Tree
 }
