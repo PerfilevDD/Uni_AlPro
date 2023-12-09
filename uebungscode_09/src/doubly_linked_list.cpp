7a848e2c5f8ad53c8a67b2669afd66474be6de44
perfilevd0 <s74dperf@uni-bonn.de>
2023-12-10 00:11:19 +0100

    Add: insert_after

---
+++ uebungscode_09/src/doubly_linked_list.cpp
@@ -0,0 +1,55 @@
+#include <datenstrukturen/doubly_linked_list.hpp>
+#include <datenstrukturen/doubly_linked_listnode.hpp>
+#include <iostream>
+#include <memory>
+
+namespace Datenstrukturen {
+    DoublyLinkedList::DoublyLinkedList() : head(nullptr), tail(nullptr) {
+        // Init. head und tail fuer die Liste
+    }
+
+    DoublyLinkedListNode::DoublyLinkedListNode(int data) : next(nullptr), prev(), data_(data) {
+        // Init. ein Knoten der Liste
+    }
+
+    DListNodeptr DoublyLinkedList::insert_front(int x) {
+        DListNodeptr n = std::make_shared<DoublyLinkedListNode>(x);
+        n->next = head;
+        // n->prev.lock() = nullptr;
+        head = n;
+        head->prev.lock() = nullptr;
+        tail = head;
+        return head;
+    }
+
+    DListNodeptr DoublyLinkedList::insert_after(const DListNodeptr& pred, int x) {
+        DListNodeptr n = std::make_shared<DoublyLinkedListNode>(x);  // Erstellen neu Knoten
+        DListNodeptr alt_pointer = pred->next;                       // Alter Pointer, der auf den naechsten Element nach pred zeigte
+
+        n->prev.lock() = pred;  // WeakPointer von n zeigt auf pred
+        n->next = alt_pointer;  // Setzen naechster Pointer nach n auf vorherigen naechsten Poninter
+        pred->next = n;         // Setzen Pointer von altem Element auf neuem Pointer
+
+        // Pruefen, falls ein naechster Pointer nach pred exestiert
+        if (alt_pointer) {
+            // Fall ja, setzen prev dieses Pointers aus n
+            alt_pointer->prev.lock() = n;
+        } else {
+            // Falls nein, dann n ist tail
+            tail = n;
+        }
+        return n;
+    }
+
+    void DoublyLinkedList::print() const {
+        DListNodeptr aktuell = head;
+
+        std::cout << tail->data_ << std::endl;
+        while (aktuell) {
+            std::cout << aktuell->data_ << " -> ";
+            aktuell = aktuell->next;
+        }
+        std::cout << "null" << std::endl;
+    }
+
+}  // namespace Datenstrukturen
