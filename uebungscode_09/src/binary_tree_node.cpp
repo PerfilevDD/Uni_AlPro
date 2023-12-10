baf47ebfea8b480f1469b38ad872722d1fec3424
perfilevd0 <s74dperf@uni-bonn.de>
2023-12-10 23:40:40 +0100

    Add: Entfernung von rechten/linken Knoten      Kommentare zu den alten Funktionen

--- uebungscode_09/src/binary_tree_node.cpp
+++ uebungscode_09/src/binary_tree_node.cpp
@@ -18,34 +18,52 @@ namespace Datenstrukturen {
     }
 
     const BinaryTreeNode::data_t BinaryTreeNode::get_data() const {
+        // Get Data
         return _data;
     }
 
     const BinaryTreeNode::data_t BinaryTreeNode::get_left_child_data() const {
+        // Get Data of left child
         return _left_child->get_data();
     }
 
     const BinaryTreeNode::data_t BinaryTreeNode::get_right_child_data() const {
+        // Get data of right child
         return _right_child->get_data();
     }
+
     BinaryTreeNode::NodeWeakPtr BinaryTreeNode::get_parent() {
-      return this->_parent;
+        // Get Pointer to parent
+        return this->_parent;
     }
 
     BinaryTreeNode::NodeSharedPtr BinaryTreeNode::get_left_child() {
+        // Get pointer to left child
         return this->_left_child;
     }
     BinaryTreeNode::NodeSharedPtr BinaryTreeNode::get_right_child() {
+        // Get pointer to right child
         return this->_right_child;
     }
     void BinaryTreeNode::set_left_child(data_t data) {
+        // Set left child
         // Hier wird die Funktion weak_from_this verwendet, siehe auch Vorlesungsfolien.
         _left_child = std::make_shared<BinaryTreeNode>(data, weak_from_this());
     }
 
     void BinaryTreeNode::set_right_child(data_t data) {
+        // Set left child
         // Hier wird die Funktion weak_from_this verwendet, siehe auch Vorlesungsfolien.
         _right_child = std::make_shared<BinaryTreeNode>(data, weak_from_this());
+    }
+
+    void BinaryTreeNode::del_left_child() {
+        // Del left child
+        this->_left_child = nullptr;
+    }
+    void BinaryTreeNode::del_right_child() {
+        // Del right child
+        this->_right_child = nullptr;
     }
 
 }  // namespace Datenstrukturen
