7c23ca5ba020806133e891e6a43dee0c07582f40
perfilevd0 <s74dperf@uni-bonn.de>
2023-12-10 20:28:49 +0100

    Add: Beispiele fuer PreOrder

--- uebungscode_09/examples/aufg2.cpp
+++ uebungscode_09/examples/aufg2.cpp
@@ -8,6 +8,48 @@ int main() {
   bt.set_root(42);
   auto root = bt.get_root();
   root->set_left_child(6500);
+
+  // Erstellen Knoten aus der Aufgabe 8
+
+    // Links von root
+    
+        // 11
+        root->set_left_child(11);
+        auto left11 = root->get_left_child();
+
+        // 9
+        left11->set_left_child(9);
+        auto left9 = left11->get_left_child();
+
+        // 0
+        left11->set_right_child(0);
+        auto right0 = left11->get_right_child();
+
+        // -6
+        left9->set_left_child(-6);
+        auto leftm6 = left9->get_left_child();
+
+        // 22
+        left9->set_right_child(22);
+        auto right22 = left9->get_right_child();
+    
+
+    // Rechts von root
+    
+        // r22
+        root->set_right_child(22);
+        auto rightr22 = root->get_right_child();
+
+        // 544
+        rightr22->set_right_child(544);
+        auto right544 = rightr22->get_right_child();
+
+        // r11
+        right544->set_left_child(11);
+        auto leftr11 = right544->get_left_child();
+
+  bt.print_preorder();
+
   std::cout
     << " " << std::to_string(root->get_data())
     << " " << std::to_string(root->get_left_child_data()) << std::endl;
@@ -17,5 +59,5 @@ int main() {
   //
   //       NUR EIN BEISPIEL FÜR EINEN ZUGRIFFSFEHLER
   // 
-  std::cout << std::to_string(root->get_right_child_data()) << std::endl;
+  //std::cout << std::to_string(root->get_right_child_data()) << std::endl;
 }
